df01a94e94654f04a0e1164fbf78935c94564862
arpitaprajapati7043 <arpitap433@gmail.com>
2024-05-06 03:41:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const apiData = require('./db.json');
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Update this with your React app's URL
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }));
+const PORT = process.env.PORT || 8383;
+
+app.get("/", (req, res) => {
+    res.send('welcome !!');
+});
+
+app.get('/products', (req, res) => {
+    res.send(apiData);
+});
+
+app.listen(PORT, () => {
+    console.log(`SERVER STARTED ON ${PORT}`);
+});
